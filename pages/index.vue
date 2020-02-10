7788d3270105e69426140fef816ef28e9cbeea94
fukuma_ryutaro <fukuma_ryutaro@weddingpark.co.jp>
2020-02-10 10:42:44 +0900

    feat: topのデザイン変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="hero" style="height: 100%">
+  <div class="hero text-center" style="height: 100%">
     <h1 class="hero__title">EisakuBoon!!</h1>
+    <p class="hero__description">
+      毎日の英作文トレーニングを通して
+      <br />
+      英語のスピーキング能力を鍛えよう
+    </p>
     <p class="hero__message">Choose a course</p>
     <div class="text-center">
-      <v-btn flat outlined large color="#fff" width="50%" to="training">
+      <v-btn
+        outlined
+        large
+        color="#fff"
+        width="50%"
+        to="training"
+        class="course__item"
+      >
         英会話
       </v-btn>
     </div>
@@ -17,17 +29,20 @@
   background: linear-gradient(to bottom, #34558b 30%, #ff6f61 100%);
 }
 .hero__title {
-  text-align: center;
   color: #fff;
   font-family: 'Fugaz One';
   margin: 100px 0;
   font-size: 3rem;
 }
+.hero__description {
+  color: #fff;
+  font-weight: bold;
+  font-family: 'M PLUS Rounded 1c';
+  margin-bottom: 100px;
+}
 .hero__message {
-  text-align: center;
   color: #fff;
   font-family: 'Fugaz One';
-  margin: 100px 0;
   font-size: 1.5rem;
 }
 </style>
